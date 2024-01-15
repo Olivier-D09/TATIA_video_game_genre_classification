4f184e244552816f2a5a4599bc8f1111877e3a56
Kazenoyama <kishan.turpin@gmail.com>
2024-01-15 14:25:11 +0100

    Columns retired, only Genre,Title and Summary are kept for the model.

--- preprocessing/prepro.py
+++ preprocessing/prepro.py
@@ -2,6 +2,7 @@ import pandas as pd;
 import matplotlib.pyplot as plt;
 import os;
 import platform;
+import seaborn as sns;
 
 if platform.system() == "Windows":
     path = "preprocessing/"
@@ -10,4 +11,17 @@ else:
 
 #Importing the dataset
 dataset = pd.read_csv(path + "games.csv");
-print(dataset.head());
+
+#Show corroleation matrix
+def show_correlation_matrix(df):
+    plt.figure(figsize=(10,10))
+    corr = df.corr()
+    sns.heatmap(corr, annot=True)
+    plt.show()
+
+data = dataset.drop(['Release Date','Team','Rating',
+                     'Times Listed','Number of Reviews','Reviews','Plays',
+                     'Playing','Backlogs','Wishlist'],axis=1)
+
+print(data.columns)
+print(data)
