4520458dffe81e7a61d61e01ba6e903a98a76afb
olivier <olivier.dalmau@etu.univ-cotedazur.fr>
2024-01-16 10:52:11 +0100

    clear name & genre from summary

--- preprocessing/prepro.py
+++ preprocessing/prepro.py
@@ -2,7 +2,7 @@ import pandas as pd;
 import matplotlib.pyplot as plt;
 import os;
 import platform;
-#import seaborn as sns;
+import seaborn as sns;
 
 if platform.system() == "Windows":
     path = "preprocessing/"
@@ -12,12 +12,59 @@ else:
 #Importing the dataset
 dataset = pd.read_csv(path + "games.csv");
 
+def get_genres(line):
+    acc = []
+    for i in line:
+        for j in i.split(', '):
+            for k in j:
+                #keep only characters
+                if not k.isalpha():
+                    j = j.replace(k,'')
+            #remove empty elements
+            if j != '':
+                #remove duplicates
+                if j not in acc:
+                    acc.append(j)
+    return acc
+
+def g_line(line):
+    acc = []
+    for j in line.split(', '):
+            for k in j:
+                #keep only characters
+                if not k.isalpha():
+                    j = j.replace(k,'')
+            #remove empty elements
+            if j != '':
+                #remove duplicates
+                if j not in acc:
+                    acc.append(j)
+    return acc
+
 #Show corroleation matrix
-# def show_correlation_matrix(df):
-#     plt.figure(figsize=(10,10))
-#     corr = df.corr()
-#     sns.heatmap(corr, annot=True)
-#     plt.show()
+def show_correlation_matrix(df):
+    plt.figure(figsize=(10,10))
+    corr = df.corr()
+    sns.heatmap(corr, annot=True)
+    plt.show()
+
+
+
+def normalize_summary(dataset):
+    for i in range(len(dataset)):
+        tmp = str(dataset['Summary'][i])
+        if dataset['Title'][i] in dataset['Summary'][i]:
+            #remove title from summary
+            tmp = tmp.replace(dataset['Title'][i],'The game')
+        #remove Genres in summary
+        gen = g_line(dataset['Genres'][i])
+        for j in gen:
+            j = str(j).lower()
+            tmp = tmp.lower()
+            if j in tmp:
+                tmp = tmp.replace(j,'')
+        dataset['Summary'][i] = tmp
+
 
 data = dataset.drop(['Release Date','Team','Rating',
                      'Times Listed','Number of Reviews','Reviews','Plays',
@@ -26,21 +73,12 @@ data = dataset.drop(['Release Date','Team','Rating',
 #print(data.columns)
 #print(data)
 
-#take all differents labels of Genres and put them in a list
-genres = []
-for i in data['Genres']:
-    for j in i.split(', '):
-        for k in j:
-            #keep only characters
-            if not k.isalpha():
-                j = j.replace(k,'')
-        #remove empty elements
-        if j != '':
-            #remove duplicates
-            if j not in genres:
-                genres.append(j)
+genres = get_genres(data['Genres'])
 
-#print length of genres
-print(len(genres))
+data['Summary'] = dataset['Summary'].astype(str)
+data['Title'] = dataset['Title'].astype(str)
 
+normalize_summary(data)
+
+data.to_csv(path + 'games_clean.csv',index=False)
 
