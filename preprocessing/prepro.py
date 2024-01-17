c9ada2023d08b2da8c36a4468cceff24dec3d01e
olivier <olivier.dalmau@etu.univ-cotedazur.fr>
2024-01-17 13:58:26 +0100

    add function to convert genres on numbers

--- preprocessing/prepro.py
+++ preprocessing/prepro.py
@@ -13,7 +13,9 @@ else:
 dataset = pd.read_csv(path + "games.csv");
 
 def get_genres(line):
-    acc = []
+    acc1 = []
+    acc2 = []
+    count = 1
     for i in line:
         for j in i.split(', '):
             for k in j:
@@ -23,11 +25,14 @@ def get_genres(line):
             #remove empty elements
             if j != '':
                 #remove duplicates
-                if j not in acc:
-                    acc.append(j)
-    return acc
+                if j not in acc1:
+                    acc1.append(j)
+                    temp = [j,count]
+                    acc2.append(temp)
+                    count += 1
+    return acc1,acc2
 
-def g_line(line):
+def genre_by_line(line):
     acc = []
     for j in line.split(', '):
             for k in j:
@@ -62,9 +67,7 @@ def normalize_summary(dataset):
 
         for x in title:
             if x.find(":") != -1 or x.isdigit():
-                print(x)
                 splited_title = title.split(x)
-                print(splited_title)
                 for k in range(len(splited_title)):
                     if splited_title[k] !="" and not splited_title[k].isdigit():
                         if splited_title[k] in tmp:
@@ -73,7 +76,7 @@ def normalize_summary(dataset):
                         return 1
 
         #remove Genres in summary
-        gen = g_line(dataset['Genres'][i])
+        gen = genre_by_line(dataset['Genres'][i])
         for j in gen:
             j = str(j).lower()
             tmp = tmp.lower()
@@ -83,19 +86,29 @@ def normalize_summary(dataset):
         dataset['Summary'][i] = tmp
 
 
-data = dataset.drop(['Release Date','Team','Rating',
-                     'Times Listed','Number of Reviews','Reviews','Plays',
-                     'Playing','Backlogs','Wishlist'],axis=1)
+def normalize_genre(data):
+    for i in range(len(data)):
+        tmp = []
+        for j in genre_by_line(data['Genres'][i]):
+            for k in numeroted_genres:
+                if j == k[0]:
+                    tmp.append(k[1])
+        data['Genres'][i] = tmp
+
+data = dataset.drop(['Release Date','Team','Rating','Times Listed','Number of Reviews','Reviews','Plays','Playing','Backlogs','Wishlist '],axis=1)
 
 #print(data.columns)
 #print(data)
 
-genres = get_genres(data['Genres'])
+genres, numeroted_genres = get_genres(data['Genres'])
+
 
 data['Summary'] = dataset['Summary'].astype(str)
 data['Title'] = dataset['Title'].astype(str)
 
 normalize_summary(data)
+normalize_genre(data)
+
 
 data.to_csv(path + 'games_clean.csv',index=False)
 
